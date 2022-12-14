4fbcea30a5eb00bfac2d3b6cec610cfd4a189711
Ivan Stoyanov <super.ludiq@gmail.com>
2022-12-14 15:26:17 +0100

    sw register fix3

--- src/index.jsx
+++ src/index.jsx
@@ -9,9 +9,9 @@ import './styles/weather.scss';
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     // we will register it after the page complete the load
-    navigator.serviceWorker.register('./service-worker.js')
-    .then((reg) => console.log('SW Register: Success', reg.scope))
-    .catch((err) => console.log('SW Register: Failed', err));
+    navigator.serviceWorker.register('/service-worker.js')
+    .then((reg) => console.log('SW Register: Success | Message: ', reg.scope))
+    .catch((err) => console.log('SW Register: Failed | Message: ', err));
   });
 }
 
